b182572410a53eb185ef13f07c1b68ae9b8db8fe
msyinmei <yin.codes@gmail.com>
2014-10-06 09:38:56 -0700

    final updates, without further advancements

--- app1.js
+++ app1.js
@@ -2,53 +2,47 @@
 // Set up players
 var player1 = {name:"Player 1",marker:"X", clicked:[]};
 var player2 = {name:"Player 2",marker:"O", clicked:[]};
-
-// var inputMarker = function(){
-// var list_items = document.querySelectorAll("div#essentials > ul > li");
-// for (var i = list_items.length - 1; i >= 0; i--) {
-// list_items[i].style.backgroundColor = "yellow";
-// list_items[i].addEventListener("click", selectItem);
-// };
-// }
-
-// var selectItem = function( event ){
-// console.log("Clicked item:" + this.innerHTML + ". Event:" + event);
-// this.innerHTML.add(inputMarker);
-// document.querySelector("img").setAttribute("src","./images/" + this.innerHTML + ".jpeg");
-// }
-
-
-// var makeMark = function (event) {
-//   var slot = document.querySelector("div#" + slot + )
-
-//   console.log("Player clicked on " + slotNum +  )
-//   list_items[i].className = "";
-
-// };
-
-// var counter = function(event) {
-// for (var i = Things.length - 1; i >= 0; i++) {
-//   Things[i]
-// };
-
-//   console.log("this function has run " + numTimes + " time(s).")
-// };
-
-
-
-// var tracker = function(){
-// if (counter%2 === 0){
-//   player2.clicked.push(currentslot);
-//   console.log("Now, Player 2 has claimed the slots:" + player2.clicked);
-// } else {
-//   player1.clicked.push(currentslot);
-//   console.log("Now, Player 1 has claimed the slots:" + player1.clicked);
-// }
-// };
+// var Wins = {r1:[s0,s2,s3],r2:[s4,s5,s6],r3:[s7,s8,s9],c1:[s0,s4,s7],c2:[s2,s5,s8],c3:[s3,s6,s9],d1:[s0,s5,s9],d2:[s3,s5,s9]};
 
 var counter = 0;
 var selectedMarker;
 var currentslot;
+
+// var checkWinnerRows = function(){
+//   if (document.querySelector(".s0").innerHTML === document.querySelector(".s1").innerHTML === document.querySelector(".s2").innerHTML ) {
+//     alert("Winner is " + document.querySelector('.s0').innerHTML);
+//     }
+//   else if (document.querySelector(".s3").innerHTML === document.querySelector(".s4").innerHTML === document.querySelector(".s5").innerHTML ) {
+//     alert("Winner is " + document.querySelector('.slot3').innerHTML);
+//     }
+//   else if (document.querySelector(".s6").innerHTML === document.querySelector(".s7").innerHTML === document.querySelector(".s8").innerHTML ) {
+//     alert("Winner is " + document.querySelector('.s6').innerHTML);
+//     }
+//     else {
+//       console.log("continue");
+//     }
+//   };
+// var checkWinnerColumns;
+// var checkWinnerDiagonals;
+
+// var CheckWins = function(){
+//   for (var i = player1.clicked.length - 1; i >= 0; i--) {
+//     Things[i]
+//   };
+// }
+
+var checkDraw;
+
+
+var tracker = function(){
+if (counter%2 === 0){
+  player2.clicked.push(currentslot);
+  console.log("Now, Player 2 has claimed the slots:" + player2.clicked);
+} else {
+  player1.clicked.push(currentslot);
+  console.log("Now, Player 1 has claimed the slots:" + player1.clicked);
+}
+};
 
 var selector = function(){
  if (counter%2 === 0){
@@ -75,24 +69,30 @@ var clickmagic = function (event) {
 } else {
   return;
 }
-//make sure not to repeat;
-  // currentslot = this.id;
-  // console.log(currentslot);
-  // tracker();
+  currentslot = this.id;
+  console.log(currentslot);
+  tracker();
+  // checkWinnerRows();
+  // checkWinnerColumns();
+  // checkWinnerDiagonals();
+  // checkDraw();
+
 };
 
 var resetmagic = function(event){
   console.log("Resetmagic initiated");
   for (var i = 0; i <=8; i++) {
-    document.querySelector("#slot" + i).innerHTML="";
+    document.querySelector("#s" + i).innerHTML="";
   }
+  player1.clicked = [];
+  player2.clicked = [];
   counter = 0;
 };
 
 var initialize = function (){
     console.log("Window is done loading page.");
     for (var i = 0; i <=8; i++) {
-    document.querySelector("#slot"+ i).addEventListener("click",clickmagic);
+    document.querySelector("#s"+ i).addEventListener("click",clickmagic);
     }
     document.querySelector(".btn").addEventListener("click",resetmagic);
 };
